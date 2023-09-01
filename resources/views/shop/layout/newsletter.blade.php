dbd78120c6e7b661fbe01abe69e1157bd0b0bacc
r0cketfuel <114667259+r0cketfuel@users.noreply.github.com>
2023-09-01 14:02:34 -0300

    Avances

--- resources/views/shop/layout/newsletter.blade.php
+++ resources/views/shop/layout/newsletter.blade.php
@@ -9,12 +9,16 @@
         <div id="newsletter-suscription-msg" style="display: none;">
             <p></p>
         </div>
-		<div class="newsletter-follow">
-			@if(session("infoComercio.facebook")  != "") <div><a href="{{session('infoComercio.facebook') }}"><i class="fa-brands fa-facebook"></i></a></div>@endif
-			@if(session("infoComercio.twitter")   != "") <div><a href="{{session('infoComercio.twitter')  }}"><i class="fa-brands fa-twitter"></i></a></div>@endif
-			@if(session("infoComercio.instagram") != "") <div><a href="{{session('infoComercio.instagram')}}"><i class="fa-brands fa-instagram"></i></a></div>@endif
-			@if(session("infoComercio.pinterest") != "") <div><a href="{{session('infoComercio.pinterest')}}"><i class="fa-brands fa-pinterest"></i></a></div>@endif
-		</div>
+
+		@if (session("infoComercio.facebook") || session("infoComercio.twitter") || session("infoComercio.instagram") || session("infoComercio.pinterest"))
+			<p>También podes seguirnos en nuestras redes:</p>
+			<div class="newsletter-follow">
+				@if(session("infoComercio.facebook")  != "") <div><a href="{{session('infoComercio.facebook') }}"><i class="fa-brands fa-facebook"></i></a></div>@endif
+				@if(session("infoComercio.twitter")   != "") <div><a href="{{session('infoComercio.twitter')  }}"><i class="fa-brands fa-twitter"></i></a></div>@endif
+				@if(session("infoComercio.instagram") != "") <div><a href="{{session('infoComercio.instagram')}}"><i class="fa-brands fa-instagram"></i></a></div>@endif
+				@if(session("infoComercio.pinterest") != "") <div><a href="{{session('infoComercio.pinterest')}}"><i class="fa-brands fa-pinterest"></i></a></div>@endif
+			</div>
+		@endif
 	</div>
 </div>
 <form id="form-suscribe" method="post" action="/suscribe">@csrf</form>
