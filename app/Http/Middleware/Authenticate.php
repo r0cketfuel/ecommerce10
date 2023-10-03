58aeee8723309af5244a2cc0a5fdd980fe50b311
r0cketfuel <unleashedwings@gmail.com>
2023-10-03 11:21:02 -0300

    Fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,7 @@ class Authenticate extends Middleware
     {
         if(!$request->expectsJson())
         {
-            if($request->is("admin.*"))
+            if($request->is("admin/*"))
             {
                 return route("admin.login");
             }
