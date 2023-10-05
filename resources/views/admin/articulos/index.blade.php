dd9ec5b7179b1d947389f0c06ffb4314beaaf7de
r0cketfuel <unleashedwings@gmail.com>
2023-10-05 13:50:27 -0300

    Avances

--- resources/views/admin/articulos/index.blade.php
+++ resources/views/admin/articulos/index.blade.php
@@ -41,52 +41,52 @@
         <table>
             <thead>
                 <tr>
-                    <th>Código</th>
-                    <th>Nombre</th>
-                    <th>Descripción</th>
-                    <th>Precio</th>
-                    <th>Moneda</th>
-                    <th>Categoría</th>
-                    <th>Subcategoría</th>
-                    <th>Visualizaciones</th>
-                    <th>Foto 1</th>
-                    <th>Foto 2</th>
-                    <th>Foto 3</th>
-                    <th>Foto 4</th>
-                    <th>Foto 5</th>
-                    <th>Foto 6</th>
-                    <th>Foto 7</th>
-                    <th>Foto 8</th>
-                    <th>Estado</th>
+                    <th class="text-center">Código</th>
+                    <th class="text-left">Nombre</th>
+                    <th class="text-left">Descripción</th>
+                    <th class="text-center">Precio</th>
+                    <th class="text-center">Moneda</th>
+                    <th class="text-left">Categoría</th>
+                    <th class="text-left">Subcategoría</th>
+                    <th class="text-center">Visualizaciones</th>
+                    <th class="text-left">Foto 1</th>
+                    <th class="text-left">Foto 2</th>
+                    <th class="text-left">Foto 3</th>
+                    <th class="text-left">Foto 4</th>
+                    <th class="text-left">Foto 5</th>
+                    <th class="text-left">Foto 6</th>
+                    <th class="text-left">Foto 7</th>
+                    <th class="text-left">Foto 8</th>
+                    <th class="text-center">Estado</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($articulos as $articulo)
                     <tr>
-                        <td>{{ $articulo->codigo }}</td>
-                        <td>{{ $articulo->nombre }}</td>
-                        <td>{{ $articulo->descripcion }}</td>
-                        <td>{{ _money($articulo->precio) }}</td>
-                        <td>{{ $articulo->moneda }}</td>
-                        <td>
+                        <td class="text-center">{{ $articulo->codigo }}</td>
+                        <td class="text-left">{{ $articulo->nombre }}</td>
+                        <td class="text-left">{{ $articulo->descripcion }}</td>
+                        <td class="text-right">{{ _money($articulo->precio) }}</td>
+                        <td class="text-center">{{ $articulo->moneda }}</td>
+                        <td class="text-left">
                             @if(isset($articulo->categoria))
                                 {{ $articulo->categoria->nombre }}
                             @endif
                         </td>
-                        <td>
+                        <td class="text-left">
                             @if(isset($articulo->subcategoria))
                                 {{ $articulo->subcategoria->nombre }}
                             @endif
                         </td>
-                        <td>{{ $articulo->visualizaciones }}</td>
-                        <td>{{ $articulo->foto_1 }}</td>
-                        <td>{{ $articulo->foto_2 }}</td>
-                        <td>{{ $articulo->foto_3 }}</td>
-                        <td>{{ $articulo->foto_4 }}</td>
-                        <td>{{ $articulo->foto_5 }}</td>
-                        <td>{{ $articulo->foto_6 }}</td>
-                        <td>{{ $articulo->foto_7 }}</td>
-                        <td>{{ $articulo->foto_8 }}</td>
+                        <td class="text-right">{{ $articulo->visualizaciones }}</td>
+                        <td class="text-left">{{ $articulo->foto_1 }}</td>
+                        <td class="text-left">{{ $articulo->foto_2 }}</td>
+                        <td class="text-left">{{ $articulo->foto_3 }}</td>
+                        <td class="text-left">{{ $articulo->foto_4 }}</td>
+                        <td class="text-left">{{ $articulo->foto_5 }}</td>
+                        <td class="text-left">{{ $articulo->foto_6 }}</td>
+                        <td class="text-left">{{ $articulo->foto_7 }}</td>
+                        <td class="text-left">{{ $articulo->foto_8 }}</td>
                         <td class="text-center">
                             <label class="switch">
                                 <input type="checkbox" id="{{ $articulo->id }}" @if($articulo->estado) checked @endif>
