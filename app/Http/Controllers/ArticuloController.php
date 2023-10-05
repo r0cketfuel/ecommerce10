42720f64d69bb778ea59999e358e7d52014e9474
r0cketfuel <unleashedwings@gmail.com>
2023-10-05 14:14:22 -0300

    Avances

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -15,7 +15,7 @@ class ArticuloController extends Controller
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
     public function index()
     {
-        $articulos = Articulo::all();
+        $articulos = Articulo::all()->where("activo", True);
 
         return view("admin.articulos.index", compact('articulos'));
     }
