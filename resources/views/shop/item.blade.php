49ad1c2e8a5c85300c2337b22d1df50cac36ddfa
r0cketfuel <unleashedwings@gmail.com>
2023-10-17 10:18:33 -0300

    Avances

--- resources/views/shop/item.blade.php
+++ resources/views/shop/item.blade.php
@@ -1,6 +1,10 @@
 @extends("shop.layout.master")
 
-@section("title","Artículo: ")
+@php
+    $title = $item->nombre;
+@endphp
+
+@section("title", $title)
 
 @section("css")
     <link rel="stylesheet"	href="{{config('constants.shop_css')}}carousel.css">
