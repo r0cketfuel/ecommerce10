b628267fbacd2ff79eba56d26170aa477be4215a
r0cketfuel <unleashedwings@gmail.com>
2023-10-17 08:12:16 -0300

    Meta tag

--- resources/views/shop/layout/master.blade.php
+++ resources/views/shop/layout/master.blade.php
@@ -2,8 +2,10 @@
 <html lang="{{str_replace('_', '-', app()->getLocale())}}">
 	<head>
         <meta charset="utf-8">
-        <meta name="viewport"   content="width=device-width, initial-scale=1.0">
-        <meta name="csrf-token" content="{{csrf_token()}}">
+        <meta name="viewport"       content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token"     content="{{csrf_token()}}">
+        <meta name="description"    content="{{ session('infoComercio.nombre') }}">
+        
         <title>@yield("title") - {{session("infoComercio.nombre")}}</title>
 
         <!-- Hojas de estilo -->
