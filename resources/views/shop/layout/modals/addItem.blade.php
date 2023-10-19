781b90c6cbebf3826940ab4eaeb785773373e36b
r0cketfuel <unleashedwings@gmail.com>
2023-10-19 11:09:31 -0300

    Avances

--- resources/views/shop/layout/modals/addItem.blade.php
+++ resources/views/shop/layout/modals/addItem.blade.php
@@ -63,7 +63,7 @@
 
                 </div>
             </div>
-            <button id="button_addToCart" class="btn-primary"><span><i class="fa-solid fa-plus"></i></span>{{ __('general.addtocart') }}</button>
+            <button id="button_addToCart" class="btn-primary"><span><i class="fa-solid fa-cart-plus"></i></span>{{ __('general.addtocart') }}</button>
         </div>
     </div>
 </div>
