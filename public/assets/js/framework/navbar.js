e47a31d58c81afa472cb24c7a52d9a7116ee9470
r0cketfuel <unleashedwings@gmail.com>
2023-10-24 08:15:57 -0300

    Avances

--- public/assets/js/framework/navbar.js
+++ public/assets/js/framework/navbar.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const menuOpenBtn = document.getElementById("responsive-nav");
-    if (menuOpenBtn)
-        menuOpenBtn.addEventListener("click", function () { sidebarOpenClose(); return false });
+    if (menuOpenBtn) {
+        menuOpenBtn.addEventListener("click", sidebarOpenClose);
+    }
 
-    const mainMenu = document.querySelectorAll(".main-menu-link");
-    for (let i = 0; i < mainMenu.length; i++)
-        mainMenu[i].addEventListener("click", function () { mainMenuClick(i); return false });
+    const mainMenuLinks = document.querySelectorAll(".main-menu-link");
+    mainMenuLinks.forEach(link => {
+        link.addEventListener("click", mainMenuClick);
+    });
 
-    const subMenu = document.querySelectorAll(".submenu-link");
-    for (let i = 0; i < subMenu.length; i++)
-        subMenu[i].addEventListener("click", function () { subMenuClick(i); return false });
-
+    const subMenuLinks = document.querySelectorAll(".submenu-link");
+    subMenuLinks.forEach(link => {
+        link.addEventListener("click", subMenuClick);
+    });
+    
 });
+
 
 //======================//
 // Sidebar Open / Close //
 //======================//
 function sidebarOpenClose() {
     let navLinks = document.querySelector(".nav-links");
-    let menuOpenBtn = document.getElementsByClassName("font-awesome-icon-bars")[0];
-    let menuCloseBtn = document.getElementsByClassName("font-awesome-icon-close")[0];
+    navLinks.style.left = navLinks.style.left === "0" ? "-100%" : "0";
+
+    let menuOpenBtn     = document.getElementsByClassName("font-awesome-icon-bars")[0];
+    let menuCloseBtn    = document.getElementsByClassName("font-awesome-icon-close")[0];
 
     menuOpenBtn.onclick = function () {
         navLinks.style.left = "0";
