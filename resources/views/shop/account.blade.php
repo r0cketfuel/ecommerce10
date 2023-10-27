0373d8cdfdacf37238c33c90c0db62994e2a9c88
r0cketfuel <unleashedwings@gmail.com>
2023-10-26 21:55:25 -0300

    Avances

--- resources/views/shop/account.blade.php
+++ resources/views/shop/account.blade.php
@@ -3,13 +3,12 @@
 @section("title","Mi cuenta")
 
 @section("css")
-@endsection
-
-@section("inlineCSS")
+    <link rel="stylesheet"	href="{{  config('constants.shop_css') }}breadcrumb.css">
+    <link rel="stylesheet"	href="{{  config('constants.framework_css') }}accordion.css">
 @endsection
 
 @section("js")
-    <script defer src="/assets/framework/js/accordion.js"></script>
+    <script defer src="{{  config('constants.framework_js') }}accordion.js"></script>
 @endsection
 
 @section("body")
@@ -27,25 +26,25 @@
                         <div class="flex">
                             <label>
                                 Apellidos
-                                <input form="form" id="apellidos" name="apellidos" value="{{session('shop.usuario.apellidos')}}">
+                                <input form="form" id="apellidos" name="apellidos" value="{{ session('shop.usuario.apellidos') }}">
                             </label>
                             <label>
                                 Nombres
-                                <input form="form" id="nombres" name="nombres" value="{{session('shop.usuario.nombres')}}">
+                                <input form="form" id="nombres" name="nombres" value="{{ session('shop.usuario.nombres') }}">
                             </label>
                         </div>
 
                         <div class="flex">
                             <label>
                                 Fecha de nacimiento
-                                <input form="form" id="fecha_nacimiento" name="fecha_nacimiento" value="{{session('shop.usuario.datos.fecha_nacimiento')}}">
+                                <input form="form" id="fecha_nacimiento" name="fecha_nacimiento" value="{{ session('shop.usuario.datos.fecha_nacimiento') }}">
                             </label>
 
                             <label>
                                 Género
                                 <select form="form" id="genero_id" name="genero_id">
                                     <option value="" disabled selected>Seleccione...</option>
-                                </select> value="{{session('shop.usuario.genero_id')}}">
+                                </select> value="{{ session('shop.usuario.genero_id') }}">
                             </label>
 
                             <label>
@@ -53,7 +52,7 @@
                                 <select form="form" id="tipo_documento_id" name="tipo_documento_id" required>
                                     <option value="" selected disabled>Seleccione</option>
                                     @foreach($tiposDocumentos as $tipoDocumento)
-                                        <option value="{{$tipoDocumento->id}}" @if(session('shop.usuario.datos.tipo_documento_id') == $tipoDocumento->id) selected @endif>{{$tipoDocumento->tipo}}</option>
+                                        <option value="{{ $tipoDocumento->id }}" @if(session('shop.usuario.datos.tipo_documento_id') == $tipoDocumento->id) selected @endif>{{$tipoDocumento->tipo}}</option>
                                     @endforeach
                                 </select>
                             </label>
@@ -61,69 +60,69 @@
 
                         <label>
                             Número de documento
-                            <input form="form" id="documento_nro" name="documento_nro" value="{{session('shop.usuario.datos.documento_nro')}}">
+                            <input form="form" id="documento_nro" name="documento_nro" value="{{ session('shop.usuario.datos.documento_nro') }}">
                         </label>
 
                         <label>
                             Cuil
-                            <input form="form" id="cuil" name="cuil" value="{{session('shop.usuario.datos.cuil')}}">
+                            <input form="form" id="cuil" name="cuil" value="{{ session('shop.usuario.datos.cuil') }}">
                         </label>
 
                         <label>
                             Cuit
-                            <input form="form" id="cuit" name="cuit" value="{{session('shop.usuario.datos.cuit')}}">
+                            <input form="form" id="cuit" name="cuit" value="{{ session('shop.usuario.datos.cuit') }}">
                         </label>
 
                         <label>
                             Domicilio
-                            <input form="form" id="domicilio" name="domicilio" value="{{session('shop.usuario.datos.domicilio')}}">
+                            <input form="form" id="domicilio" name="domicilio" value="{{ session('shop.usuario.datos.domicilio') }}">
                         </label>
 
                         <div class="flex">
                             <label>
                                 Domicilio número
-                                <input form="form" type="text" id="domicilio_nro" name="domicilio_nro" required value="{{session('shop.usuario.datos.domicilio_nro')}}">
+                                <input form="form" type="text" id="domicilio_nro" name="domicilio_nro" required value="{{ session('shop.usuario.datos.domicilio_nro') }}">
                             </label>
                             
                             <label>
                                 Domicilio piso
-                                <input form="form" type="text" id="domicilio_piso" name="domicilio_piso" value="{{session('shop.usuario.datos.domicilio_piso')}}">
+                                <input form="form" type="text" id="domicilio_piso" name="domicilio_piso" value="{{ session('shop.usuario.datos.domicilio_piso') }}">
                             </label>
 
                             <label>
                                 Domicilio depto
-                                <input form="form" type="text" id="domicilio_depto" name="domicilio_depto" value="{{session('shop.usuario.datos.domicilio_depto')}}">
+                                <input form="form" type="text" id="domicilio_depto" name="domicilio_depto" value="{{ session('shop.usuario.datos.domicilio_depto') }}">
                             </label>
                         </div>
 
                         <label>
                             Localidad
-                            <input form="form" type="text" id="localidad" name="localidad" required value="{{session('shop.usuario.datos.localidad')}}">
+                            <input form="form" type="text" id="localidad" name="localidad" required value="{{ session('shop.usuario.datos.localidad') }}">
                         </label>
 
                         <label>
                             Codigo postal
-                            <input form="form" type="text" id="codigo_postal" name="codigo_postal" required value="{{session('shop.usuario.datos.codigo_postal')}}">
+                            <input form="form" type="text" id="codigo_postal" name="codigo_postal" required value="{{ session('shop.usuario.datos.codigo_postal') }}">
                         </label>
 
                         <label>
                             Teléfono fijo
-                            <input form="form" type="text" id="telefono_fijo" name="telefono_fijo" required value="{{session('shop.usuario.datos.telefono_fijo')}}">
+                            <input form="form" type="text" id="telefono_fijo" name="telefono_fijo" required value="{{ session('shop.usuario.datos.telefono_fijo') }}">
                         </label>
 
                         <label>
                             Teléfono celular
-                            <input form="form" type="text" id="telefono_celular" name="telefono_celular" required value="{{session('shop.usuario.datos.telefono_celular')}}">
+                            <input form="form" type="text" id="telefono_celular" name="telefono_celular" required value="{{ session('shop.usuario.datos.telefono_celular') }}">
                         </label>
 
                         <label>
                             Teléfono alternativo
-                            <input form="form" type="text" id="telefono_alt" name="telefono_alt" value="{{session('shop.usuario.datos.telefono_alt')}}">
+                            <input form="form" type="text" id="telefono_alt" name="telefono_alt" value="{{ session('shop.usuario.datos.telefono_alt') }}">
                         </label>
 
                         <label>
                             Correo electrónico
-                            <input form="form" type="text" id="email" name="email" required value="{{session('shop.usuario.datos.email')}}">
+                            <input form="form" type="text" id="email" name="email" required value="{{ session('shop.usuario.datos.email') }}">
                         </label>
 
                         <button form="form" name="form" class="btn-primary">Guardar cambios</button>
