ac115d06d9a31adf5a3283a094907ef3ce69e5f4
r0cketfuel <unleashedwings@gmail.com>
2023-11-02 14:22:40 -0300

    Avances

--- database/seeders/ImagenArticuloSeeder.php
+++ database/seeders/ImagenArticuloSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\ImagenArticulo;
 
 class ImagenArticuloSeeder extends Seeder
 {
@@ -12,6 +13,46 @@ class ImagenArticuloSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        ImagenArticulo::create([
+            "articulo_id"   => 1,
+            "ruta"          => "1.png",
+            "descripcion"   => "Imágen 1 del artículo 1"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 1,
+            "ruta"          => "2.png",
+            "descripcion"   => "Imágen 2 del artículo 1"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 1,
+            "ruta"          => "3.png",
+            "descripcion"   => "Imágen 3 del artículo 1"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 1,
+            "ruta"          => "4.png",
+            "descripcion"   => "Imágen 4 del artículo 1"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 1,
+            "ruta"          => "5.png",
+            "descripcion"   => "Imágen 5 del artículo 1"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 2,
+            "ruta"          => "1.png",
+            "descripcion"   => "Imágen 1 del artículo 2"
+        ]);
+
+        ImagenArticulo::create([
+            "articulo_id"   => 2,
+            "ruta"          => "2.png",
+            "descripcion"   => "Imágen 2 del artículo 2"
+        ]);
     }
 }
