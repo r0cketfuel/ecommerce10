58506d44e4d6e10ec948a30c2853c1f651bd77aa
r0cketfuel <unleashedwings@gmail.com>
2023-11-06 10:33:16 -0300

    Avances

--- resources/views/shop/layout/whatsappBubble.blade.php
+++ resources/views/shop/layout/whatsappBubble.blade.php
@@ -1,7 +1,7 @@
 <div class="chat">
-    <a href="https://wa.me/{{ session()->get('infoComercio.telefono1') }}" target="_blank">
+    <a href="https://wa.me/{{ session('infoComercio.telefono_1') }}" target="_blank">
         <div class="chat-bubble chat-bubble-whatsapp">
-            <i class="chat-icon fa-brands fa-whatsapp" aria-hidden="true"></i>
+            <i class="chat-icon fa-brands fa-whatsapp"></i>
         </div>
     </a>
 </div>
