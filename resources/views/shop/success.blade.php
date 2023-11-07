02b7c85cf5f225ec1748008b3e98b69df3629b59
r0cketfuel <unleashedwings@gmail.com>
2023-11-07 11:07:59 -0300

    Avances

--- resources/views/shop/success.blade.php
+++ resources/views/shop/success.blade.php
@@ -22,14 +22,13 @@
     <!-- Contenido de la página -->
     <div class="main-container">
         <div class="flex">
-            <div class="panel" style="margin: auto; flex: 0 1 500px;">
+            <div class="panel" style="margin: auto; flex: 0 1 500px; padding: 40px;">
                 <div class="grid text-center">
-                    <h1><span style="color: rgb(100,220,100);"><i class="fa-regular fa-circle-check fa-4x"></i></span><br><br>COMPRA FINALIZADA</h1>
-                    <h3>Muchas gracias por tu compra</h3>
-                    <h4>Su pedido se registró bajo el número: </h4>
-                    <h4>Puedes ver los detalles de la compra haciendo click <a href="/shop/details?order=">AQUI</a></h4>
+                    <h2><span style="color: rgb(100,220,100);"><i class="fa-regular fa-circle-check fa-6x"></i></span><br><br>Muchas gracias por su compra!</h2>
+                    <h4>Su pedido se registró bajo el número: {{ $order }}</h4>
+                    <h4>Puedes ver los detalles de la compra haciendo click <a href="/shop/details?order={{ $order }}">AQUI</a></h4>
                     <h4>Tambien hemos enviado un correo con los detalles de la compra</h4>
-                    <button>Seguir comprando</button>
+                        <button>Seguir comprando</button>
                 </div>
             </div>
         </div>
