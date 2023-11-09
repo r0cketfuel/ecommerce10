a189a338d218079c537f07891990938e9c7428dd
r0cketfuel <unleashedwings@gmail.com>
2023-11-09 09:36:04 -0300

    Avances

--- app/Models/ImagenArticulo.php
+++ app/Models/ImagenArticulo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class ImagenArticulo extends Model
 {
@@ -20,4 +21,11 @@ class ImagenArticulo extends Model
         "ruta",
         "descripcion"
     ];
+    
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+    public function articulo()
+    {
+        return($this->belongsTo(Articulo::class));
+    }
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 }
