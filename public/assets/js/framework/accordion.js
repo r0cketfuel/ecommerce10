276f9c9077494e8150bc37d49a661d36654d3743
r0cketfuel <unleashedwings@gmail.com>
2023-11-10 21:56:52 -0300

    Cambios

--- public/assets/js/framework/accordion.js
+++ public/assets/js/framework/accordion.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let acc = document.getElementsByClassName("accordion-header");
+    let acc = document.getElementsByClassName("accordion");
 
     for(let i=0;i<acc.length;i++)
         acc[i].addEventListener("click", function () { this.classList.toggle("accordion-active"); });
