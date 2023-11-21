5096d8fe01b089bd1f40d81c88eeac314393bffd
r0cketfuel <unleashedwings@gmail.com>
2023-11-21 12:45:24 -0300

    Cambios

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -13,44 +13,11 @@ class CategoriaSeeder extends Seeder
      */
     public function run(): void
     {
-        Categoria::create([
-            "nombre"        => "Categoría 1",
-            "descripcion"   => "Descripción Categoría 1"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 2",
-            "descripcion"   => "Descripción Categoría 2"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 3",
-            "descripcion"   => "Descripción Categoría 3"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 4",
-            "descripcion"   => "Descripción Categoría 4"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 5",
-            "descripcion"   => "Descripción Categoría 5"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 6",
-            "descripcion"   => "Descripción Categoría 6"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 7",
-            "descripcion"   => "Descripción Categoría 7"
-        ]);
-
-        Categoria::create([
-            "nombre"        => "Categoría 8",
-            "descripcion"   => "Descripción Categoría 8"
-        ]);
+        for ($i = 1; $i <= 10; $i++) {
+            Categoria::create([
+                "nombre"        => "Categoría $i",
+                "descripcion"   => "Descripción Categoría $i"
+            ]);
+        }
     }
 }
