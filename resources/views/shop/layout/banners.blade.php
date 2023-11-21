af58a194eb70dc5f544acab3d38b09f2d1e4bc31
r0cketfuel <unleashedwings@gmail.com>
2023-11-21 09:33:14 -0300

    Cambios

--- resources/views/shop/layout/banners.blade.php
+++ resources/views/shop/layout/banners.blade.php
@@ -1,11 +1,11 @@
 <div class="slideshow">
-    @for($i=0;$i<count($banners);$i++)
+    @for($i = 0; $i < count($banners); $i++)
         <div class="slide">
             <a href="shop/banners/{{ $banners[$i]['link'] }}">
                 <img src="{{ $banners[$i]['imagen'] }}" alt="imagen">
             </a>
         </div>
     @endfor
-    <button class="btn btn-next">&gt;</button>
-    <button class="btn btn-prev">&lt;</button>
+    <button class="btn btn-next"><i class="fa-solid fa-chevron-right"></i></button>
+    <button class="btn btn-prev"><i class="fa-solid fa-chevron-left"></i></button>
 </div>
