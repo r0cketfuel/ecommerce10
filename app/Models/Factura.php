b074cce7b780743c3ca08a6b6778f185785870b8
r0cketfuel <unleashedwings@gmail.com>
2023-11-23 08:33:19 -0300

    Avances

--- app/Models/Factura.php
+++ app/Models/Factura.php
@@ -38,22 +38,6 @@ class Factura extends Model
         "estado_id"
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-    ];
-
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
     public static function generarFactura(array $parametros): Factura
     {
