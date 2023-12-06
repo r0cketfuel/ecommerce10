9e588c445b46b501b75453ce2981855868ce2753
r0cketfuel <unleashedwings@gmail.com>
2023-12-06 14:28:09 -0300

    Fix

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -327,7 +327,7 @@
             }
 
             function mostrarPanelEnvio() {
-                panelEnvios.style.display = 'block'
+                panelEnvios.style.display = 'flex'
             }
 
             function ocultarPanelEnvio() {
