d27281877087647c5bedb80068bb0fc2bdae216a
r0cketfuel <unleashedwings@gmail.com>
2023-12-07 09:17:48 -0300

    Cambios

--- resources/views/emails/signup.blade.php
+++ resources/views/emails/signup.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{str_replace('_', '-', app()->getLocale())}}">
+<html lang="es">
 	<head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bienvenido/a a nuestro sitio</title>
     </head>
-    <body>
+    <div class="body">
         <p>Hola {{ $apellidos }}, {{ $nombres }}</p>
         
         <p>¡Bienvenido/a a nuestro sitio! Estamos emocionados de tenerte como parte de nuestra comunidad.</p>
@@ -19,5 +19,5 @@
 
         <p>Saludos,</p>
         <p>Tu Equipo</p>
-    </body>
+    </div>
 </html>
