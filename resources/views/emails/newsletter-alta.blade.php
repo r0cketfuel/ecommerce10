4f916626cf8beb0f46e084cb780023e623ecfc9d
r0cketfuel <unleashedwings@gmail.com>
2023-12-11 12:40:56 -0300

    Avances

---
+++ resources/views/emails/newsletter-alta.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bienvenido/a a nuestro sitio</title>
+    </head>
+    <div class="body">
+        <p>Hola {{ $apellidos }}, {{ $nombres }}</p>
+        
+        <p>¡Bienvenido/a a nuestro sitio! Estamos emocionados de tenerte como parte de nuestra comunidad.</p>
+
+        <p>Gracias por suscribirte a nuestro Newsletter!</p>
+
+        <p>Saludos,</p>
+        <p>Tu Equipo</p>
+    </div>
+</html>
