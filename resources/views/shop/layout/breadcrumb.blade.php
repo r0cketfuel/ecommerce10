0b59f333355b2c3f03e1421f146961462490aa7a
r0cketfuel <unleashedwings@gmail.com>
2023-12-15 13:17:54 -0300

    Cambios

--- resources/views/shop/layout/breadcrumb.blade.php
+++ resources/views/shop/layout/breadcrumb.blade.php
@@ -1,12 +1,12 @@
 @if(isset($breadcrumbs))
     <div class="breadcrumb">
-        <a href="/shop"><i class="fa-solid fa-house-chimney fa-sm"></i> Home</a>
+        <a href="/shop"><i class="fa-solid fa-house-chimney fa-xs"></i> Home</a>
 
         @foreach($breadcrumbs as $breadcrumb)
-            <i class="fa-solid fa-chevron-right fa-sm"></i> <a href="{{ $breadcrumb['link'] }}">{{ $breadcrumb['title'] }}</a>
+            <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="{{ $breadcrumb['link'] }}">{{ $breadcrumb['title'] }}</a>
         @endforeach
 
-        <i class="fa-solid fa-chevron-right fa-sm"></i> {{ $title }}
+        <i class="fa-solid fa-chevron-right fa-xs"></i> {{ $title }}
     </div>
 @endif
 
