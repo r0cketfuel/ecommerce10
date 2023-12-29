972fd543864bf37818cb456a4281b1f7d86c8914
r0cketfuel <unleashedwings@gmail.com>
2023-12-29 09:43:35 -0300

    Cambios

--- resources/views/shop/layout/whatsappBubble.blade.php
+++ resources/views/shop/layout/whatsappBubble.blade.php
@@ -1,7 +1,9 @@
-<div class="chat">
-    <a href="https://wa.me/{{ session('infoComercio.whatsapp') }}" target="_blank">
-        <div class="chat-bubble chat-bubble-whatsapp">
-            <i class="chat-icon fa-brands fa-whatsapp"></i>
-        </div>
-    </a>
-</div>
+@if(session('infoComercio.whatsapp'))
+    <div class="chat">
+        <a href="https://wa.me/{{ session('infoComercio.whatsapp') }}" target="_blank">
+            <div class="chat-bubble chat-bubble-whatsapp">
+                <i class="chat-icon fa-brands fa-whatsapp"></i>
+            </div>
+        </a>
+    </div>
+@endif
