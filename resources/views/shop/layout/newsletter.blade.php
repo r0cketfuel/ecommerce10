e1b77bbfc6c511bf09730d27068593bd05a32da6
r0cketfuel <unleashedwings@gmail.com>
2024-01-12 10:32:34 -0300

    Animación de iconos

--- resources/views/shop/layout/newsletter.blade.php
+++ resources/views/shop/layout/newsletter.blade.php
@@ -15,11 +15,11 @@
         @if (session("infoComercio.facebook") || session("infoComercio.twitter") || session("infoComercio.instagram") || session("infoComercio.pinterest"))
             <p>{{ __('general.follow_msg') }}:</p>
             <div class="newsletter-follow">
-                @if(session("infoComercio.facebook"))	<div><a href="{{session('infoComercio.facebook') }}"><i class="fa-brands fa-facebook"></i></a></div>@endif
-                @if(session("infoComercio.twitter")) 	<div><a href="{{session('infoComercio.twitter')  }}"><i class="fa-brands fa-x-twitter"></i></a></div>@endif
-                @if(session("infoComercio.instagram")) 	<div><a href="{{session('infoComercio.instagram')}}"><i class="fa-brands fa-instagram"></i></a></div>@endif
-                @if(session("infoComercio.pinterest")) 	<div><a href="{{session('infoComercio.pinterest')}}"><i class="fa-brands fa-pinterest"></i></a></div>@endif
-				@if(session("infoComercio.tiktok")) 	<div><a href="{{session('infoComercio.tiktok')}}"><i class="fa-brands fa-tiktok"></i></a></div>@endif
+                @if(session("infoComercio.facebook"))	<div><a href="{{session('infoComercio.facebook') }}"><i class="fa-brands fa-facebook fa-xl"></i></a></div>@endif
+                @if(session("infoComercio.twitter")) 	<div><a href="{{session('infoComercio.twitter')  }}"><i class="fa-brands fa-x-twitter fa-xl"></i></a></div>@endif
+                @if(session("infoComercio.instagram")) 	<div><a href="{{session('infoComercio.instagram')}}"><i class="fa-brands fa-instagram fa-xl"></i></a></div>@endif
+                @if(session("infoComercio.pinterest")) 	<div><a href="{{session('infoComercio.pinterest')}}"><i class="fa-brands fa-pinterest fa-xl"></i></a></div>@endif
+				@if(session("infoComercio.tiktok")) 	<div><a href="{{session('infoComercio.tiktok')}}"><i class="fa-brands fa-tiktok fa-xl"></i></a></div>@endif
             </div>
         @endif
     </div>
