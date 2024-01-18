1441220b8e017a3484b63a45565a8741c1de9f83
r0cketfuel <unleashedwings@gmail.com>
2024-01-18 08:47:14 -0300

    Avances

--- resources/views/admin/subcategorias/index.blade.php
+++ resources/views/admin/subcategorias/index.blade.php
@@ -24,6 +24,7 @@
                 <th class="text-left">Categoría</th>
                 <th class="text-left">Nombre</th>
                 <th class="text-left">Descripción</th>
+                <th class="text-left">Acciones</th>
             </tr>
         </thead>
         <tbody>
@@ -32,8 +33,50 @@
                 <td>{{ $subcategoria->categoria->nombre }}</td>
                 <td>{{ $subcategoria->nombre }}</td>
                 <td>{{ $subcategoria->descripcion }}</td>
+                <td class="text-center">
+                    <a href="/admin/subcategorias/{{ $subcategoria->id }}"><i class="fa-solid fa-eye"></i></a>
+                    <button type="button" class="deleteButton" id="{{ $subcategoria->id }}"><i class="fa-solid fa-trash"></i></button>
+                </td>
             </tr>
             @endforeach
         </tbody>
     </table>
+@endsection
+
+@section("scripts")
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+
+            const buttons = [...document.querySelectorAll("button[type='button']")];
+
+            for (let i = 0; i < buttons.length; ++i)
+            {
+                buttons[i].addEventListener("click", function () {
+                    const articleId = buttons[i].getAttribute('id');
+                    baja(articleId);
+                    return false;
+                });
+            }
+
+            async function baja(id)
+            {
+                try {
+                    const response = await fetch(`/api/subcategorias/${id}`, {
+                        method: "DELETE",
+                        headers: {
+                            'Accept':       'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                    });
+
+                    const responseBody = await response.json();
+                    console.log(responseBody);
+                }
+                catch (error)
+                {
+                    console.error('Error en la solicitud:', error);
+                }
+            }
+        });
+    </script>
 @endsection
