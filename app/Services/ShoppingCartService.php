65f6751bd7808c0affcde37b9fa70e0d5dcf67c3
r0cketfuel <unleashedwings@gmail.com>
2024-01-24 12:56:29 -0300

    Avances

--- app/Services/ShoppingCartService.php
+++ app/Services/ShoppingCartService.php
@@ -7,52 +7,56 @@
 
     class ShoppingCartService
     {
+        const SESSION_CART_KEY          = "shop.usuario.carrito";
+        const SESSION_ITEMS_KEY         = "shop.usuario.carrito.items";
+        const SESSION_CONFIRMATION_KEY  = "shop.usuario.carrito.confirmacion";
+
 		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-		public function __construct()
-		{
-            if(!session()->has("shop.usuario.carrito"))
+        public function __construct()
+        {
+            if(!session()->has(self::SESSION_CART_KEY))
                 $this->clear();
-		}
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-		public function clear(): int
-		{
+        }
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+        public function clear(): int
+        {
 			//=========================================//
 			// Método que limpia el carrito de compras //
 			//=========================================//
-            session()->put("shop.usuario.carrito", array());
-            session()->put("shop.usuario.carrito.items", array());
-            session()->put("shop.usuario.carrito.confirmacion", False);
 
-            return($this->totalItems());
-		}
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+            session()->put(self::SESSION_CART_KEY,          array());
+            session()->put(self::SESSION_ITEMS_KEY,         array());
+            session()->put(self::SESSION_CONFIRMATION_KEY,  False);
+
+            return $this->totalItems();
+        }
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function itemIndex(int $id, array $opciones = array()): int
         {
             //==============================================================//
             // Método que devuelve el índice del arreglo donde esta el item //
             //==============================================================//
 
-            for($i=0;$i<count(session("shop.usuario.carrito.items"));$i++)
-                if(session("shop.usuario.carrito.items.$i.id") == $id)
-                    if(empty(array_diff_assoc(session("shop.usuario.carrito.items.$i.opciones"), $opciones)))
-                        return($i);
-            
-            return(-1);
+            for($i=0;$i<count(session(self::SESSION_ITEMS_KEY));$i++)
+                if(session(self::SESSION_ITEMS_KEY)[$i]["id"] == $id && empty(array_diff_assoc(session(self::SESSION_ITEMS_KEY)[$i]["opciones"], $opciones)))
+                    return $i;
+
+            return -1;
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function updateCart(int $id, int $cantidad, array $opciones = array())
         {
             //============================================//
             // Método que actualiza el carrito de compras //
             //============================================//
-            
+
             if($id>0 && $cantidad>=0)
             {
                 $atributoArticulo   = new AtributoArticulo;
                 $atributosId        = $atributoArticulo->search($id, $opciones)->first()->id;
-                
+
                 $limiteCompra       = $atributoArticulo->maximoCompra($id, $opciones);
-                
+
                 $index = $this->itemIndex($id, $opciones);
 
                 if($index<0)
@@ -61,7 +65,9 @@
                     // ITEM NO EXISTE EN EL CARRITO - AGREGAR //
                     //----------------------------------------//
                     if($cantidad<=$limiteCompra && $cantidad>0)
+                    {
                         $this->addItem($id, $atributosId, $cantidad, $opciones);
+                    }
                 }
                 else
                 {
@@ -79,9 +85,9 @@
                 }
             }
 
-            return($this->totalItems());
+            return $this->totalItems();
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function addItem(int $id, int $atributosId, int $cantidad, array $opciones = array())
         {
             //=====================================================//
@@ -90,23 +96,23 @@
 
             $info = Articulo::info($id);
 
-            session()->push("shop.usuario.carrito.items", array(
-                "id" 		    => $id,
-                "atributos_id"  => $atributosId,
-                "cantidad"	    => $cantidad,
-                "precio"	    => $info["precio"],
-                "subtotal"	    => $info["precio"] * $cantidad,
-                "opciones"      => $opciones
+            session()->push(self::SESSION_ITEMS_KEY, array(
+                "id"             => $id,
+                "atributos_id"   => $atributosId,
+                "cantidad"       => $cantidad,
+                "precio"         => $info["precio"],
+                "subtotal"       => $info["precio"] * $cantidad,
+                "opciones"       => $opciones
             ));
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function updateItem(int $index, int $cantidad)
         {
             //======================================================================//
             // Método que actualiza la cantidad de un item en el carrito de compras //
             //======================================================================//
 
-            session()->put("shop.usuario.carrito.items.$index.cantidad", $cantidad);
+            session()->put(self::SESSION_ITEMS_KEY . ".$index.cantidad", $cantidad);
         }
 		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function removeItem(int $index)
@@ -115,36 +121,38 @@
             // Método que elimina un item del carrito de compras //
             //===================================================//
 
-            array_splice(session("shop.usuario.carrito.items"), $index, 1);
+            array_splice(session(self::SESSION_ITEMS_KEY), $index, 1);
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-		public function checkOut(): array
-		{
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+        public function checkOut(): array
+        {
 			//=========================================================================================//
 			// Método que devuelve un array con los items del carrito, información, total y subtotales //
 			//=========================================================================================//
 
-			$chekOutArray = array(
-				"items"	=> session("shop.usuario.carrito.items"),
-				"total"	=> $this->total()
-			);
+            $chekOutArray = array(
+                "items" => session(self::SESSION_ITEMS_KEY),
+                "total" => $this->total()
+            );
 
-			return($chekOutArray);
-		}
+            return $chekOutArray;
+        }
 		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-		public function total(): float
-		{
+        public function total(): float
+        {
             //==========================================================//
 			// Método que calcula el monto total del carrito de compras //
 			//==========================================================//
 
-			$total = 0;
-			for($i=0;$i<count(session("shop.usuario.carrito.items"));++$i)
-				$total  = $total + session("shop.usuario.carrito.items.$i.subtotal");
+            $total = 0;
 
-			return($total);
-		}
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+            for($i=0;$i<count(session(self::SESSION_ITEMS_KEY));$i++)
+                $total = $total + session(self::SESSION_ITEMS_KEY)[$i]["subtotal"];
+
+
+            return $total;
+        }
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function itemQuantity(int $id, array $opciones = array()): int
         {
             //==========================================================================//
@@ -153,11 +161,11 @@
 
             $index = $this->itemIndex($id, $opciones);
             if($index>=0)
-                return(session("shop.usuario.carrito.items.$index.cantidad"));
+                return session(self::SESSION_ITEMS_KEY)[$index]["cantidad"];
 
-			return(-1);
+            return -1;
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
         public function totalItems(): int
         {
             //====================================================//
@@ -165,10 +173,10 @@
             //====================================================//
 
             $total = 0;
-            for($i=0;$i<count(session("shop.usuario.carrito.items"));++$i)
-                $total += session("shop.usuario.carrito.items.$i.cantidad");
+            for($i=0;$i<count(session(self::SESSION_ITEMS_KEY));$i++)
+                $total += session(self::SESSION_ITEMS_KEY)[$i]["cantidad"];
 
-            return($total);
+            return $total;
         }
-        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
     }
