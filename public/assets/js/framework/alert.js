fb85079a7f7c27aa4c123e2d25c68314b3ca642c
r0cketfuel <unleashedwings@gmail.com>
2024-01-25 10:49:04 -0300

    Cambios

--- public/assets/js/framework/alert.js
+++ public/assets/js/framework/alert.js
@@ -1,14 +1,19 @@
-function createAlertContainer(className, message)
-{
+const ALERT_TIMEOUT = 5000;
+
+function createAlertContainer(className, message) {
     const alertContainer = document.createElement("div");
     alertContainer.className = "alert " + className;
 
     const closeBtn = document.createElement("span");
     closeBtn.className = "closebtn";
     closeBtn.innerHTML = "&times;";
-    
+
     closeBtn.onclick = function () {
-        this.parentElement.style.display = 'none';
+        // Al hacer clic en el botón de cerrar, agregar animación de salida y ocultar después de la animación
+        alertContainer.style.animation = 'fade-out 0.3s';
+        setTimeout(function () {
+            alertContainer.style.display = 'none';
+        }, 300);
     };
 
     alertContainer.innerHTML = message;
@@ -17,9 +22,16 @@ function createAlertContainer(className, message)
     return alertContainer;
 }
 
-function displayAlert(className, message)
-{
+function displayAlert(className, message) {
     const alertContainer = createAlertContainer(className, message);
     document.body.appendChild(alertContainer);
     alertContainer.style.display = 'flex';
+    
+    // Configurar el temporizador para ocultar la alerta después de ALERT_TIMEOUT milisegundos
+    setTimeout(function () {
+        alertContainer.style.animation = 'fade-out 0.3s';
+        setTimeout(function () {
+            alertContainer.style.display = 'none';
+        }, 300);
+    }, ALERT_TIMEOUT);
 }
