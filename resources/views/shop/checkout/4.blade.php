bfe1b057c1e137b56eb04b77886d84d3e254f652
r0cketfuel <unleashedwings@gmail.com>
2024-02-27 10:31:54 -0300

    Identación

--- resources/views/shop/checkout/4.blade.php
+++ resources/views/shop/checkout/4.blade.php
@@ -10,61 +10,52 @@
         <div class="panel-content">
 
             <!-- Contenido -->
-                    @for($i=0;$i < count($checkout["items"]);++$i)
+            @for($i=0;$i < count($checkout["items"]);++$i)
 
-                        @php
-                            $id             = $checkout["items"][$i]["id"];
-                            $atributosId    = $checkout["items"][$i]["atributos_id"];
-                            $nombre         = $checkout["items"][$i]["nombre"];
-                            $precio         = $checkout["items"][$i]["precio"];
-                            $imagen         = $checkout["items"][$i]["imagen"] ? $checkout["items"][$i]["imagen"] : asset('images/content/no-image.png');
-                            $cantidad 	    = $checkout["items"][$i]["cantidad"];
-                            $subtotal       = $checkout["items"][$i]["subtotal"];
-                            $total          = $checkout["total"];
+                @php
+                    $id             = $checkout["items"][$i]["id"];
+                    $atributosId    = $checkout["items"][$i]["atributos_id"];
+                    $nombre         = $checkout["items"][$i]["nombre"];
+                    $precio         = $checkout["items"][$i]["precio"];
+                    $imagen         = $checkout["items"][$i]["imagen"] ? $checkout["items"][$i]["imagen"] : asset('images/content/no-image.png');
+                    $cantidad 	    = $checkout["items"][$i]["cantidad"];
+                    $subtotal       = $checkout["items"][$i]["subtotal"];
+                    $total          = $checkout["total"];
 
-                            $precio         = _money($precio);
-                            $subtotal       = _money($subtotal);
-                            $total          = _money($total);
+                    $precio         = _money($precio);
+                    $subtotal       = _money($subtotal);
+                    $total          = _money($total);
 
-                            $opciones       = $checkout["items"][$i]["opciones"];
-                            $talle_id       = $checkout["items"][$i]["opciones"]["talle_id"]    ?? null;
-                            $color          = $checkout["items"][$i]["opciones"]["color"]       ?? null;
-                        @endphp
+                    $opciones       = $checkout["items"][$i]["opciones"];
+                    $talle_id       = $checkout["items"][$i]["opciones"]["talle_id"]    ?? null;
+                    $color          = $checkout["items"][$i]["opciones"]["color"]       ?? null;
+                @endphp
 
-                        <div class="grid grid-cols-2">
-                            <p>Artículo:</p><p>{{ $nombre }}</p>
-                            <p>Cantidad:</p><p>{{ $cantidad }}</p>
-                            <p>Talle:</p><p>{{ $talle_id }}</p>
-                            <p>Color:</p><p>{{ $color }}</p>
-                            <p>Precio:</p><p>{{ $precio }}</p>
-                            <p>Subtotal:</p><p>{{ $subtotal }}</p>
-                        </div>
-                    @endfor
-
-            <!-- Contenido -->
-
-                <div class="panel-title panel-title-underlined">Datos facturación</div>
-
-                    <div class="grid grid-cols-2">
-                        <p>Apellidos:</p><p>{{ session('shop.checkout.datos.apellidos') }}</p>
-                        <p>Nombres:</p><p>{{ session('shop.checkout.datos.nombres') }}</p>
-                        <p>Número de documento:</p><p>{{ session('shop.checkout.datos.documento_nro') }}</p>
-                        <p>Correo electrónico:</p><p>{{ session('shop.checkout.datos.email') }}</p>
-                    </div>
-
-
+                <div class="grid grid-cols-2">
+                    <p>Artículo:</p><p>{{ $nombre }}</p>
+                    <p>Cantidad:</p><p>{{ $cantidad }}</p>
+                    <p>Talle:</p><p>{{ $talle_id }}</p>
+                    <p>Color:</p><p>{{ $color }}</p>
+                    <p>Precio:</p><p>{{ $precio }}</p>
+                    <p>Subtotal:</p><p>{{ $subtotal }}</p>
+                </div>
+            @endfor
 
             <!-- Contenido -->
+            <div class="panel-title panel-title-underlined">Datos facturación</div>
+            <div class="grid grid-cols-2">
+                <p>Apellidos:</p><p>{{ session('shop.checkout.datos.apellidos') }}</p>
+                <p>Nombres:</p><p>{{ session('shop.checkout.datos.nombres') }}</p>
+                <p>Número de documento:</p><p>{{ session('shop.checkout.datos.documento_nro') }}</p>
+                <p>Correo electrónico:</p><p>{{ session('shop.checkout.datos.email') }}</p>
+            </div>
 
-                <div class="panel-title panel-title-underlined">Forma de pago y envío</div>
-
-                    <div class="grid grid-cols-2">
-                        <p>Forma de pago:</p><p>{{ session('shop.checkout.medio_pago.medio') }}</p>
-                        <p>Envío:</p><p>{{ session('shop.checkout.medio_envio.medio') }}</p>
-                    </div>
-
-
-
+            <!-- Contenido -->
+            <div class="panel-title panel-title-underlined">Forma de pago y envío</div>
+            <div class="grid grid-cols-2">
+                <p>Forma de pago:</p><p>{{ session('shop.checkout.medio_pago.medio') }}</p>
+                <p>Envío:</p><p>{{ session('shop.checkout.medio_envio.medio') }}</p>
+            </div>
         </div>
     </div>
 </div>
