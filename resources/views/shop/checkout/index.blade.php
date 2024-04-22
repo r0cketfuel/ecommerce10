0a6a8f818e9fa6a77bfd2d1b59a2d85fa888374a
r0cketfuel <unleashedwings@gmail.com>
2024-04-22 09:05:16 -0300

    Avances

--- resources/views/shop/checkout/index.blade.php
+++ resources/views/shop/checkout/index.blade.php
@@ -13,6 +13,8 @@
 
 @section("css")
 	<link rel="stylesheet"	href="{{ config('constants.shop_css') }}productCards.css">
+	<link rel="stylesheet"	href="{{ config('constants.shop_css') }}progress-indicator.css">
+	<link rel="stylesheet"	href="{{ config('constants.shop_css') }}carousel-forms.css">
     <link rel="stylesheet"	href="{{ config('constants.framework_css') }}panel.css">
     <link rel="stylesheet"	href="{{ config('constants.framework_css') }}modal.css">
 @endsection
@@ -30,85 +32,8 @@
             }
         }
 
-		.carousel-container {
-			flex: 					1;
-			overflow: 				hidden;
-		}
-
-		.carousel-slider {
-			display: 				flex;
-			flex-flow: 				row nowrap;
-			width: 					calc(400% + 300px);
-			gap:					100px;
-		}
-
-		.carousel-slide {
-			flex: 					1;
-			transition: 			all 0.5s ease-in-out;
-		}
-
-		.btnPrev,
-		.btnNext {
-			padding:				8px 16px;
-			border: 				1px solid rgb(200,200,200);
-			border-radius:  		4px;
-			width:					unset;
-		}
-
-		.btnPrev:not(.btn-primary):hover,
-		.btnNext:not(.btn-primary):hover {
-			cursor: 				pointer;
-			background-color: 		rgba(0, 0, 0, 0.05);
-		}
-
 		.panel {
 			box-shadow: 			unset;
-		}
-
-		.progress-indicator {
-			user-select: 			none;
-			display: 				grid;
-			grid-template-columns: 	repeat(4, 1fr);
-			grid-template-rows: 	repeat(2, auto);
-			justify-content: 		space-between;
-			justify-items: 			center;
-			gap: 					5px;
-			text-align: 			center;
-			background-color: 		#fafafa;
-			border: 				1px solid rgb(220, 220, 220);
-			border-radius: 			4px;
-			padding: 				25px;
-		}
-
-		.progress-indicator .step-number {
-			border-radius: 			50%;
-			height: 				32px;
-			width: 					32px;
-			border: 				3px solid rgb(220, 220, 220);
-			display: 				flex;
-			justify-content: 		center;
-			align-items: 			center;
-			font-weight: 			bold;
-		}
-
-		.progress-indicator .step-number.current-step {
-			border: 				3px solid rgb(30, 200, 30);
-		}
-
-		.progress-indicator .success {
-			background-color: 		rgb(80, 210, 80);
-			border: 				3px solid rgb(30, 200, 30);
-			color: 					white;
-		}
-
-		/* .progress-indicator div {
-			height: 				3px;
-			flex:					1;
-			background-color: 		rgb(200,200,200);
-		} */
-		
-		.progress-indicator div.success {
-			background-color: 		rgb(80, 210, 80);
 		}
 
 		.container {
