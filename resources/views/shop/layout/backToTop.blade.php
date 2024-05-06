ed67d55fe8f0d12039c79b3eab3fbbc9bc49ae8b
r0cketfuel <unleashedwings@gmail.com>
2024-05-06 09:47:01 -0300

    Remuevo div sobrante

--- resources/views/shop/layout/backToTop.blade.php
+++ resources/views/shop/layout/backToTop.blade.php
@@ -1,7 +1,5 @@
 <div id="backToTop" class="backToTop">
-    <div>
-        <div class="backToTop-icon">
-            <i class="fa-solid fa-chevron-up"></i>
-        </div>
+    <div class="backToTop-icon">
+        <i class="fa-solid fa-chevron-up"></i>
     </div>
 </div>
